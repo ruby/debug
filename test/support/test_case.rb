981ad0e69f66581e5b91487138c216f40c269d28
Naoto Ono <nono19@students.desu.edu>
2021-05-21 14:21:33 +0900

    create test_case.rb -extend test-unit's base

---
+++ test/support/test_case.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require 'tempfile'
+
+require_relative 'utils'
+
+module DEBUGGER__
+  class TestCase < Test::Unit::TestCase
+    include TestUtils
+
+    def teardown
+      remove_temp_file
+    end
+
+    def remove_temp_file
+      File.unlink(@temp_file)
+      @temp_file = nil
+    end
+
+    def temp_file_path
+      @temp_file.path
+    end
+
+    def write_temp_file(program)
+      @temp_file = Tempfile.create(%w[debugger .rb])
+      @temp_file.write(program)
+      @temp_file.close
+    end
+  end
+end
