392c82f0faf646d2f341d79d104ccc32187a23ed
st0012 <stan001212@gmail.com>
2021-06-02 09:19:35 +0800

    Merge breakpoint tests

--- test/debug/break_test.rb
+++ test/debug/break_test.rb
@@ -78,6 +78,33 @@ module DEBUGGER__
     end
   end
 
+  class BreakAtCMethodsTest < TestCase
+    def program
+      <<~RUBY
+      1| a = 1
+      2|
+      3| a.abs
+      RUBY
+    end
+
+    def test_debugger_stops_when_the_exception_raised
+      debug_code(program) do
+        type 'b Integer#abs'
+        type 'continue'
+
+        if RUBY_VERSION.to_f >= 3.0
+          assert_line_text(/Integer#abs at <internal:/)
+        else
+          # it doesn't show any source before Ruby 3.0
+          assert_line_text(/<main>/)
+        end
+
+        type 'quit'
+        type 'y'
+      end
+    end
+  end
+
   #
   # Tests adding breakpoints to empty methods
   #
