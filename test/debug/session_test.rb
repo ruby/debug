30093a731aceefe93e3249921898954a617cc97f
st0012 <stan001212@gmail.com>
2021-06-04 22:59:28 +0800

    Add tests for manual session start

---
+++ test/debug/session_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative '../support/test_case'
+
+module DEBUGGER__
+  class ManualSessionStartTest < TestCase
+    def program
+      <<~RUBY
+       1| a = 1
+       2| b = 2
+       3| require "debug"
+       4| DEBUGGER__.console
+       5| c = 3
+       6| binding.bp
+       7| "foo"
+      RUBY
+    end
+
+    def test_session_starts_manually
+      debug_code(program, boot_options: "") do
+        assert_line_num(5)
+        type 'quit'
+        type 'y'
+      end
+    end
+
+    def test_later_breakpoint_fires_correctly
+      debug_code(program, boot_options: "") do
+        assert_line_num(5)
+        type 'c'
+        assert_line_num(6)
+        type 'quit'
+        type 'y'
+      end
+    end
+  end
+end
