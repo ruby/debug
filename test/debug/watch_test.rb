526845327e8533df19af8c9a4456759f273ee24d
st0012 <stan001212@gmail.com>
2021-06-04 13:55:12 +0800

    Add line numbers to watch test

--- test/debug/watch_test.rb
+++ test/debug/watch_test.rb
@@ -29,27 +29,27 @@ module DEBUGGER__
   class ObjectInstanceVariableWatchingTest < TestCase
     def program
       <<~RUBY
-        class Student
-          attr_accessor :name
-
-          def initialize(name)
-            @name = name
-            binding.bp(command: "watch @name")
-          end
-        end
-
-        class Teacher
-          attr_accessor :name
-
-          def initialize(name)
-            @name = name
-          end
-        end
-
-        s1 = Student.new("John")
-        t1 = Teacher.new("Jane") # it shouldn't stop for this
-        s1.name = "Josh"
-        "foo"
+         1| class Student
+         2|   attr_accessor :name
+         3|
+         4|   def initialize(name)
+         5|     @name = name
+         6|     binding.bp(command: "watch @name")
+         7|   end
+         8| end
+         9|
+        10| class Teacher
+        11|   attr_accessor :name
+        12|
+        13|   def initialize(name)
+        14|     @name = name
+        15|   end
+        16| end
+        17|
+        18| s1 = Student.new("John")
+        19| t1 = Teacher.new("Jane") # it shouldn't stop for this
+        20| s1.name = "Josh"
+        21| "foo"
       RUBY
     end
 
