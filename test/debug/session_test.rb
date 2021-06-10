ea986496011a3f298eb56e6d0af7cd470d7f51e7
st0012 <stan001212@gmail.com>
2021-06-10 12:25:18 +0800

    Add tests for require 'debug/run' scenario

--- test/debug/session_test.rb
+++ test/debug/session_test.rb
@@ -3,13 +3,45 @@
 require_relative '../support/test_case'
 
 module DEBUGGER__
-  class ManualSessionStartTest < TestCase
+  class ConsoleStartTest < TestCase
     def program
       <<~RUBY
        1| a = 1
        2| b = 2
        3| require "debug"
        4| DEBUGGER__.console
+       5| c = 3
+       6| binding.bp
+       7| "foo"
+      RUBY
+    end
+
+    def test_session_starts_manually
+      debug_code(program, boot_options: "") do
+        assert_line_num(5)
+        type 'quit'
+        type 'y'
+      end
+    end
+
+    def test_later_breakpoint_fires_correctly
+      debug_code(program, boot_options: "") do
+        assert_line_num(5)
+        type 'c'
+        assert_line_num(6)
+        type 'quit'
+        type 'y'
+      end
+    end
+  end
+
+  class RequireStartTest < TestCase
+    def program
+      <<~RUBY
+       1| a = 1
+       2| b = 2
+       3| require "debug/run"
+       4|
        5| c = 3
        6| binding.bp
        7| "foo"
