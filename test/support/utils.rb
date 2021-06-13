5db9ae2706b79648f7ced887a4458f0df2c7a47b
ono-max <nono19@students.desu.edu>
2021-06-13 19:55:51 +0900

    use assert_block to output clearer fail logs

--- test/support/utils.rb
+++ test/support/utils.rb
@@ -12,13 +12,17 @@ module DEBUGGER__
 
     def assert_line_num(expected)
       @queue.push(Proc.new {
-        assert_equal(expected, @internal_info['line'], create_message("Expected line number to be #{expected}, but was #{@internal_info['line']}"))
+        msg = ["Expected line number to be #{expected}, but was #{@internal_info['line']}"] + @backlog
+        assert_block(msg) { expected == @internal_info['line'] }
       })
     end
 
     def assert_line_text(expected)
       @queue.push(Proc.new {
-        assert_match(expected, @last_backlog[2..].join, create_message("Expected to include #{expected}"))
+        result = @last_backlog[2..].join
+        expected = Regexp.escape(expected) if expected.is_a?(String)
+        msg = ["Expected to include `#{expected}` in\n(\n#{result})\n"] + @backlog
+        assert_block(msg) { result.match? expected }
       })
     end
 
