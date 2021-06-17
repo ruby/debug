2ba36a8050ca1cf1622df80afabf099ce44a45db
st0012 <stan001212@gmail.com>
2021-06-17 14:02:00 +0800

    Add assert_no_line_test helper

--- test/support/assertions.rb
+++ test/support/assertions.rb
@@ -11,11 +11,32 @@ module DEBUGGER__
 
     def assert_line_text(expected)
       @queue.push(Proc.new {
-        result = @last_backlog[1..].join
+        result = collect_recent_backlog
         expected = Regexp.escape(expected) if expected.is_a?(String)
         msg = "Expected to include `#{expected}` in\n(\n#{result})\n"
-        assert_block(MessageCreationController.new { create_message(msg) }) { result.match? expected }
+
+        assert_block(MessageCreationController.new { create_message(msg) }) do
+          result.match? expected
+        end
       })
+    end
+
+    def assert_no_line_text(expected)
+      @queue.push(Proc.new {
+        result = collect_recent_backlog
+        expected = Regexp.escape(expected) if expected.is_a?(String)
+        msg = "Expected not to include `#{expected}` in\n(\n#{result})\n"
+
+        assert_block(MessageCreationController.new { create_message(msg) }) do
+          !result.match? expected
+        end
+      })
+    end
+
+    private
+
+    def collect_recent_backlog
+      @last_backlog[1..].join
     end
   end
 end
