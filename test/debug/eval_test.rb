ec7cb4f1893f240b9afbdb06e92d6f39cc7122a8
st0012 <stan001212@gmail.com>
2021-06-23 11:55:43 +0800

    Add eval tests

---
+++ test/debug/eval_test.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative '../support/test_case'
+
+module DEBUGGER__
+  class EvalTest < TestCase
+    def program
+      <<~RUBY
+     1| a = "foo"
+     2| b = "bar"
+     3| c = 3
+     4| __END__
+      RUBY
+    end
+
+    def test_eval_evaluates_method_call
+      debug_code(program) do
+        type 'b 3'
+        type 'continue'
+        type 'e a.upcase!'
+        type 'p a'
+        assert_line_text(/"FOO"/)
+        type 'q!'
+      end
+    end
+
+    def test_eval_evaluates_computation_and_assignment
+      debug_code(program) do
+        type 'b 3'
+        type 'continue'
+        type 'e b = a + b'
+        type 'p b'
+        assert_line_text(/"foobar"/)
+        type 'q!'
+      end
+    end
+  end
+end
