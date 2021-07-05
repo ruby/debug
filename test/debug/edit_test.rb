23d85ba80641aa68fc05526b827f60197f9c4db9
st0012 <stan001212@gmail.com>
2021-07-05 23:48:35 +0800

    Add edit tests

---
+++ test/debug/edit_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../support/test_case'
+
+module DEBUGGER__
+  class EditTest < TestCase
+    def program
+      <<~RUBY
+      1| a = 1
+      RUBY
+    end
+
+    def test_edit_opens_the_editor
+      ENV["EDITOR"] = "null_editor"
+
+      debug_code(program, remote: false) do
+        type "edit"
+        assert_line_text(/command: null_editor/)
+        type "continue"
+      end
+    end
+
+    def test_edit_shows_warning_when_the_file_can_not_be_found
+      ENV["EDITOR"] = "null_editor"
+
+      debug_code(program, remote: false) do
+        type "edit foo.rb"
+        assert_line_text(/not found/)
+        type "continue"
+      end
+    end
+
+    def test_edit_shows_warning_when_editor_env_is_not_set
+      ENV["EDITOR"] = nil
+
+      debug_code(program, remote: false) do
+        type "edit"
+        assert_line_text(/can not find editor setting/)
+        type "continue"
+      end
+    end
+  end
+end
