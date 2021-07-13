51f0b0264f50cbc22e19a15100d8e67518af9c3a
st0012 <stan001212@gmail.com>
2021-07-13 09:58:34 +0800

    Add tests for binding.bp with pre/do options

--- test/debug/bp_test.rb
+++ test/debug/bp_test.rb
@@ -25,12 +25,12 @@ module DEBUGGER__
     end
   end
 
-  class BindingBPWithCommandTest < TestCase
+  class BindingBPWithPreCommandTest < TestCase
     def program
       <<~RUBY
      1| class Foo
      2|   def bar
-     3|     binding.bp(do: "continue")
+     3|     binding.bp(pre: "p 'aaaaa'")
      4|     baz
      5|   end
      6|
@@ -46,8 +46,51 @@ module DEBUGGER__
     def test_breakpoint_execute_command_argument_correctly
       debug_code(program) do
         type 'continue'
+        assert_line_text('Foo#bar')
+        assert_line_text(/aaaaa/)
+        # should stay at Foo#bar
+        assert_no_line_text(/Foo#baz/)
+
+        type 'continue'
         assert_line_text('Foo#baz')
+        type 'continue'
+      end
+    end
+
+    def test_debugger_doesnt_complain_about_duplicated_breakpoint
+      debug_code(program) do
+        type 'continue'
+        assert_no_line_text(/duplicated breakpoint:/)
         type 'q!'
+      end
+    end
+  end
+
+  class BindingBPWithDoCommandTest < TestCase
+    def program
+      <<~RUBY
+     1| class Foo
+     2|   def bar
+     3|     binding.bp(do: "p 'aaaaa'")
+     4|     baz
+     5|   end
+     6|
+     7|   def baz
+     8|     binding.bp
+     9|   end
+    10| end
+    11|
+    12| Foo.new.bar
+      RUBY
+    end
+
+    def test_breakpoint_execute_command_argument_correctly
+      debug_code(program) do
+        type 'continue'
+        assert_line_text(/aaaaa/)
+        # should move on to the next bp
+        assert_line_text('Foo#baz')
+        type 'continue'
       end
     end
 
