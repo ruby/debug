8459daa16e439516d1e1a20331fb5dda4683cc82
ono-max <nono19@students.desu.edu>
2021-07-14 17:54:21 +0900

    Move delete test from break_test.rb to delete_test.rb

--- test/debug/break_test.rb
+++ test/debug/break_test.rb
@@ -331,40 +331,4 @@ module DEBUGGER__
       end
     end
   end
-
-  class DeleteTest < TestCase
-    def program
-      <<~RUBY
-     1| a = 1
-     2| b = 2
-     3| c = 3
-     4| d = 4
-      RUBY
-    end
-
-    def test_delete_deletes_all_breakpoints_by_default
-      debug_code(program) do
-        type "break 2"
-        type "break 3"
-
-        type "delete"
-        type "y" # confirm deletion
-
-        type "continue"
-      end
-    end
-
-    def test_delete_deletes_a_specific_breakpoint
-      debug_code(program) do
-        type "break 2"
-        type "break 3"
-
-        type "delete 0"
-
-        type "continue"
-        assert_line_num(3)
-        type "q!"
-      end
-    end
-  end
 end
