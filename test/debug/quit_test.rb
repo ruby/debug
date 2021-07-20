4091f636ae7a0a8d8d4a9e3467b287ecabf7e0ab
ono-max <nono19@students.desu.edu>
2021-07-21 08:12:20 +0900

    Add test cases for quit command

---
+++ test/debug/quit_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative '../support/test_case'
+
+module DEBUGGER__
+  class QuitTest < TestCase
+    def program
+      <<~RUBY
+        1| a=1
+      RUBY
+    end
+
+    def test_quit_quits_debugger_process_if_confirmed
+      debug_code(program) do
+        type 'q'
+        assert_line_text(/Really quit\? \[Y\/n\]/)
+        type 'y'
+      end
+    end
+
+    def test_quit_does_not_quit_debugger_process_if_not_confirmed
+      debug_code(program) do
+        type 'q'
+        assert_line_text(/Really quit\? \[Y\/n\]/)
+        type 'n'
+        type 'q!'
+      end
+    end
+
+    def test_quit_with_exclamation_mark_quits_immediately_debugger_process
+      debug_code(program) do
+        type 'q!'
+      end
+    end
+  end
+end
