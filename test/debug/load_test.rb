2e5482064aa27d20d1432abf8e13ad064199ae9d
Koichi Sasada <ko1@atdot.net>
2021-09-14 00:28:41 +0900

    prevent to load lib/debug.rb
    old lib/debug.rb should not be allowed to load.
    `require 'debug'` is needed to update the `$LOADED_FEATURE` cache
    (maybe it is a workaround for the MRI's issue)
    
    fix #275

---
+++ test/debug/load_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative '../support/test_case'
+
+module DEBUGGER__
+  class LoadTest < TestCase
+    def program
+      <<~RUBY
+      1| r = require 'debug'
+      2| binding.break
+      RUBY
+    end
+
+    def test_require_debug_should_return_false
+      debug_code(program) do
+        type "c"
+        type "p r"
+        assert_line_text('false')
+        type 'q!'
+      end
+    end
+  end
+end
+
+
