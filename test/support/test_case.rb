828a680ddc1a1bdd5b8b329d2743bd1932d95505
Koichi Sasada <ko1@atdot.net>
2021-10-27 04:13:10 +0900

    refactoring test/support/util.rb
    * define `run_rdbg` method
    * rename `run_rdbg_on...` to `debug_code_on...` because they are
      special methods for `debug_code` method.
    * `TestInfo` now has `prompt_pattern`
    * use `remote_info` term instead of `remote_debuggee_info`
    * introduce `RemoteInfo` to represent remote debuggee process
    * remove `RUBY_DEBUG_TEST_NO_REMOTE` and use port=0 (read port from output)
    * move `temp_file_path`, `write_temp_file` to util.rb
    * choose free TCP/IP port instead of use 12345

--- test/support/test_case.rb
+++ test/support/test_case.rb
@@ -19,15 +19,5 @@ module DEBUGGER__
       File.unlink(@temp_file) if @temp_file
       @temp_file = nil
     end
-
-    def temp_file_path
-      @temp_file.path
-    end
-
-    def write_temp_file(program)
-      @temp_file = Tempfile.create(%w[debugger .rb])
-      @temp_file.write(program)
-      @temp_file.close
-    end
   end
 end
