2d4d65fe0d305976f441cdcf6368c12197937989
st0012 <stan001212@gmail.com>
2021-10-28 09:37:30 +0100

    Add init script option test

--- test/debug/rdbg_option_test.rb
+++ test/debug/rdbg_option_test.rb
@@ -35,4 +35,68 @@ module DEBUGGER__
       end
     end
   end
+
+  class InitScriptTest < TestCase
+    TEMPFILE_BASENAME = __FILE__.hash.abs.to_s(16)
+
+    def with_init_script(filename)
+      t = Tempfile.create(filename).tap do |f|
+        f.write(init_script)
+        f.close
+      end
+      yield t
+    ensure
+      File.unlink t if t
+    end
+
+    class CommandScriptTest < InitScriptTest
+      def init_script
+        <<~CMD
+        catch RuntimeError
+        CMD
+      end
+
+      def program
+        <<~RUBY
+        1| raise "foo"
+        RUBY
+      end
+
+      def test_debugger_executes_the_file_as_commands
+        with_init_script([TEMPFILE_BASENAME]) do |init_script|
+          run_rdbg(program, options: "-x #{init_script.path}") do
+            type "c"
+            assert_line_text(/Stop by #0  BP - Catch  "RuntimeError"/)
+            type "q!"
+          end
+        end
+      end
+    end
+
+    class RubyScriptTest < InitScriptTest
+      def init_script
+        <<~CMD
+        def foo
+          "foo"
+        end
+        CMD
+      end
+
+      def program
+        <<~RUBY
+        1| 123
+        RUBY
+      end
+
+      def test_debugger_executes_the_file_as_ruby
+        with_init_script([TEMPFILE_BASENAME, ".rb"]) do |init_script|
+          run_rdbg(program, options: "-x #{init_script.path}") do
+            type "foo + 'bar'"
+            assert_line_text(/foobar/)
+            type "q!"
+          end
+        end
+      end
+    end
+  end
 end
