5df93aae1986442c4ecf676cfcdf92e9e2eee28b
Koichi Sasada <ko1@atdot.net>
2022-02-21 15:30:13 +0900

    Use RubyVM.script_lines if available.

--- lib/debug/source_repository.rb
+++ lib/debug/source_repository.rb
@@ -4,72 +4,109 @@ require_relative 'color'
 
 module DEBUGGER__
   class SourceRepository
-    SrcInfo = Struct.new(:src, :colored)
-
-    def initialize
-      @files = {} # filename => SrcInfo
-    end
-
-    def add iseq, src
-      if (path = iseq.absolute_path) && File.exist?(path)
-        add_path path
-      elsif src
-        add_iseq iseq, src
-      end
-    end
-
-    def all_iseq iseq, rs = []
-      rs << iseq
-      iseq.each_child{|ci|
-        all_iseq(ci, rs)
-      }
-      rs
-    end
-
-    private def add_iseq iseq, src
-      line = iseq.first_line
-      if line > 1
-        src = ("\n" * (line - 1)) + src
-      end
-      si = SrcInfo.new(src.lines)
-
-      all_iseq(iseq).each{|e|
-        e.instance_variable_set(:@debugger_si, si)
-        e.freeze
-      }
-    end
-
-    private def add_path path
-      src = File.read(path)
-      src = src.gsub("\r\n", "\n") # CRLF -> LF
-      @files[path] = SrcInfo.new(src.lines)
-    rescue SystemCallError
-    end
-
-    private def get_si iseq
-      return unless iseq
-
-      if iseq.instance_variable_defined?(:@debugger_si)
-        iseq.instance_variable_get(:@debugger_si)
-      elsif @files.has_key?(path = iseq.absolute_path)
-        @files[path]
-      elsif path
-        add_path(path)
-      end
-    end
-
-    def get iseq
-      if si = get_si(iseq)
-        si.src
-      end
-    end
-
     include Color
 
-    def get_colored iseq
-      if si = get_si(iseq)
-        si.colored || begin
-          si.colored = colorize_code(si.src.join).lines
+    if RubyVM.respond_to? :keep_script_lines
+      # Ruby 3.1 and later
+      RubyVM.keep_script_lines = true
+      require 'objspace'
+
+      def initialize
+        # cache
+        @cmap = ObjectSpace::WeakMap.new
+      end
+
+      def add iseq, src
+        # do nothing
+      end
+
+      def get iseq
+        return unless iseq
+
+        if lines = iseq.script_lines&.map(&:chomp)
+          lines
+        else
+          nil
+        end
+      end
+
+      def get_colored iseq
+        if lines = @cmap[iseq]
+          lines
+        else
+          if src_lines = get(iseq)
+            @cmap[iseq] = colorize_code(src_lines.join("\n")).lines
+          else
+            nil
+          end
+        end
+      end
+    else
+      # ruby 3.0 or erlier
+      SrcInfo = Struct.new(:src, :colored)
+
+      def initialize
+        @files = {} # filename => SrcInfo
+      end
+
+      def add iseq, src
+        if (path = iseq.absolute_path) && File.exist?(path)
+          add_path path
+        elsif src
+          add_iseq iseq, src
+        end
+      end
+
+      private def all_iseq iseq, rs = []
+        rs << iseq
+        iseq.each_child{|ci|
+          all_iseq(ci, rs)
+        }
+        rs
+      end
+
+      private def add_iseq iseq, src
+        line = iseq.first_line
+        if line > 1
+          src = ("\n" * (line - 1)) + src
+        end
+        si = SrcInfo.new(src.lines)
+        all_iseq(iseq).each{|e|
+          e.instance_variable_set(:@debugger_si, si)
+          e.freeze
+        }
+      end
+
+      private def add_path path
+        src = File.read(path)
+        src = src.gsub("\r\n", "\n") # CRLF -> LF
+        @files[path] = SrcInfo.new(src.lines)
+      rescue SystemCallError
+      end
+
+      private def get_si iseq
+        return unless iseq
+      
+        if iseq.instance_variable_defined?(:@debugger_si)
+          iseq.instance_variable_get(:@debugger_si)
+        elsif @files.has_key?(path = iseq.absolute_path)
+          @files[path]
+        elsif path
+          add_path(path)
+        end
+      end
+
+      def get iseq
+        if si = get_si(iseq)
+          si.src
+        end
+      end
+
+      def get_colored iseq
+        if si = get_si(iseq)
+          si.colored || begin
+            si.colored = colorize_code(si.src.join).lines
+          end
         end
       end
     end
