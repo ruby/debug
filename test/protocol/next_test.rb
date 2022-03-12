c2a4d71228932f3affa04649eb2a36bde3ce985e
st0012 <stan001212@gmail.com>
2022-03-12 13:01:54 +0000

    Apply locals check to control flow tests

--- test/protocol/next_test.rb
+++ test/protocol/next_test.rb
@@ -15,17 +15,44 @@ module DEBUGGER__
       8|   bar = Bar.new
       9| end
     RUBY
-    
+
     def test_next_goes_to_the_next_statement
       run_protocol_scenario PROGRAM do
         req_next
         assert_line_num 2
+
+        assert_locals_result(
+          [
+            { name: "%self", value: "Foo", type: "Module" },
+            { name: "bar", value: "nil", type: "NilClass" }
+          ]
+        )
         req_next
         assert_line_num 3
+
+        assert_locals_result(
+          [
+            { name: "%self", value: "Foo::Bar", type: "Class" },
+          ]
+        )
         req_next
         assert_line_num 7
+
+        assert_locals_result(
+          [
+            { name: "%self", value: "Foo", type: "Module" },
+            { name: "bar", value: "nil", type: "NilClass" }
+          ]
+        )
         req_next
         assert_line_num 8
+
+        assert_locals_result(
+          [
+            { name: "%self", value: "Foo", type: "Module" },
+            { name: "bar", value: "nil", type: "NilClass" }
+          ]
+        )
         req_terminate_debuggee
       end
     end
