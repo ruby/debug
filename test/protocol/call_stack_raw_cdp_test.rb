5ca06bea5a9c46592922f20400ad32c0a90b1538
ono-max <onoto1998@gmail.com>
2022-03-13 19:48:36 +0900

    Add the protocol-based test case for callFrames in CDP

---
+++ test/protocol/call_stack_raw_cdp_test.rb
@@ -0,0 +1,611 @@
+# frozen_string_literal: true
+
+require_relative '../support/test_case'
+
+module DEBUGGER__
+  
+  class CallStackTest1647167458 < TestCase
+    PROGRAM = <<~RUBY
+      1| module Foo
+      2|   class Bar
+      3|     def self.a
+      4|       "hello"
+      5|     end
+      6|   end
+      7|   Bar.a
+      8|   bar = Bar.new
+      9| end
+
+    RUBY
+    
+    def test_1647167458
+      run_cdp_scenario PROGRAM do
+        [
+          *INITIALIZE_CDP_MSGS,
+          {
+            method: "Debugger.scriptParsed",
+            params: {
+              scriptId: /.+/,
+              url: /.+/,
+              startLine: 0,
+              startColumn: 0,
+              endLine: 9,
+              endColumn: 0,
+              executionContextId: 1,
+              hash: /.+/
+            }
+          },
+          {
+            method: "Debugger.paused",
+            params: {
+              reason: "other",
+              callFrames: [
+                {
+                  callFrameId: /.+/,
+                  functionName: "<main>",
+                  functionLocation: {
+                    lineNumber: 0,
+                    scriptId: /.+/
+                  },
+                  location: {
+                    lineNumber: 0,
+                    scriptId: /.+/
+                  },
+                  url: /.+/,
+                  scopeChain: [
+                    {
+                      type: "local",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "script",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "global",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    }
+                  ],
+                  this: {
+                    type: "object"
+                  }
+                }
+              ]
+            }
+          },
+          {
+            id: 8,
+            method: "Debugger.getScriptSource",
+            params: {
+              scriptId: "1"
+            }
+          },
+          {
+            id: 8,
+            result: {
+              scriptSource: "module Foo\n  class Bar\n    def self.a\n      \"hello\"\n    end\n  end\n  Bar.a\n  bar = Bar.new\nend\n"
+            }
+          },
+          {
+            id: 9,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "0:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 9,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Object"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 10,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "0:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 10,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Object"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 11,
+            method: "Debugger.setBreakpointsActive",
+            params: {
+              active: true
+            }
+          },
+          {
+            id: 11,
+            result: {
+            }
+          },
+          {
+            id: 12,
+            method: "Debugger.setBreakpointByUrl",
+            params: {
+              lineNumber: 3,
+              urlRegex: "#{File.realpath(temp_file_path)}|file://#{File.realpath(temp_file_path)}",
+              columnNumber: 0,
+              condition: ""
+            }
+          },
+          {
+            id: 12,
+            result: {
+              breakpointId: /.+/,
+              locations: [
+                {
+                  scriptId: /.+/,
+                  lineNumber: 3
+                }
+              ]
+            }
+          },
+          {
+            id: 13,
+            method: "Debugger.getPossibleBreakpoints",
+            params: {
+              start: {
+                scriptId: "1",
+                lineNumber: 3,
+                columnNumber: 0
+              },
+              end: {
+                scriptId: "1",
+                lineNumber: 3,
+                columnNumber: 13
+              },
+              restrictToFunction: false
+            }
+          },
+          {
+            id: 13,
+            result: {
+              locations: [
+                {
+                  scriptId: /.+/,
+                  lineNumber: 3
+                }
+              ]
+            }
+          },
+          {
+            id: 14,
+            method: "Debugger.resume",
+            params: {
+              terminateOnResume: false
+            }
+          },
+          {
+            id: 14,
+            result: {
+            }
+          },
+          {
+            method: "Debugger.resumed",
+            params: {
+            }
+          },
+          {
+            method: "Debugger.scriptParsed",
+            params: {
+              scriptId: /.+/,
+              url: /.+/,
+              startLine: 0,
+              startColumn: 0,
+              endLine: 9,
+              endColumn: 0,
+              executionContextId: 1,
+              hash: /.+/
+            }
+          },
+          {
+            method: "Debugger.scriptParsed",
+            params: {
+              scriptId: /.+/,
+              url: /.+/,
+              startLine: 0,
+              startColumn: 0,
+              endLine: 9,
+              endColumn: 0,
+              executionContextId: 1,
+              hash: /.+/
+            }
+          },
+          {
+            method: "Debugger.scriptParsed",
+            params: {
+              scriptId: /.+/,
+              url: /.+/,
+              startLine: 0,
+              startColumn: 0,
+              endLine: 9,
+              endColumn: 0,
+              executionContextId: 1,
+              hash: /.+/
+            }
+          },
+          {
+            method: "Debugger.paused",
+            params: {
+              reason: "other",
+              callFrames: [
+                {
+                  callFrameId: /.+/,
+                  functionName: "Foo::Bar.a",
+                  functionLocation: {
+                    lineNumber: 2,
+                    scriptId: /.+/
+                  },
+                  location: {
+                    lineNumber: 3,
+                    scriptId: /.+/
+                  },
+                  url: /.+/,
+                  scopeChain: [
+                    {
+                      type: "local",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "script",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "global",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    }
+                  ],
+                  this: {
+                    type: "object"
+                  }
+                },
+                {
+                  callFrameId: /.+/,
+                  functionName: "<module:Foo>",
+                  functionLocation: {
+                    lineNumber: 0,
+                    scriptId: /.+/
+                  },
+                  location: {
+                    lineNumber: 6,
+                    scriptId: /.+/
+                  },
+                  url: /.+/,
+                  scopeChain: [
+                    {
+                      type: "local",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "script",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "global",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    }
+                  ],
+                  this: {
+                    type: "object"
+                  }
+                },
+                {
+                  callFrameId: /.+/,
+                  functionName: "<main>",
+                  functionLocation: {
+                    lineNumber: 0,
+                    scriptId: /.+/
+                  },
+                  location: {
+                    lineNumber: 0,
+                    scriptId: /.+/
+                  },
+                  url: /.+/,
+                  scopeChain: [
+                    {
+                      type: "local",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "script",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    },
+                    {
+                      type: "global",
+                      object: {
+                        type: "object",
+                        objectId: /.+/
+                      }
+                    }
+                  ],
+                  this: {
+                    type: "object"
+                  }
+                }
+              ]
+            }
+          },
+          {
+            id: 15,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "0:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 16,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "0:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 15,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Class"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 16,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Class"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 17,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "1:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 18,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "1:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 17,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Module"
+                  },
+                  configurable: true,
+                  enumerable: true
+                },
+                {
+                  name: "bar",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "NilClass"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 18,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Module"
+                  },
+                  configurable: true,
+                  enumerable: true
+                },
+                {
+                  name: "bar",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "NilClass"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 19,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "2:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 20,
+            method: "Runtime.getProperties",
+            params: {
+              objectId: "2:local",
+              ownProperties: false,
+              accessorPropertiesOnly: false,
+              nonIndexedPropertiesOnly: false,
+              generatePreview: true
+            }
+          },
+          {
+            id: 19,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Object"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 20,
+            result: {
+              result: [
+                {
+                  name: "%self",
+                  value: {
+                    type: "object",
+                    description: /.+/,
+                    objectId: /.+/,
+                    className: "Object"
+                  },
+                  configurable: true,
+                  enumerable: true
+                }
+              ]
+            }
+          },
+          {
+            id: 21,
+            method: "Debugger.resume",
+            params: {
+              terminateOnResume: false
+            }
+          },
+          {
+            id: 21,
+            result: {
+            }
+          }
+        ]
+      end
+    end
+  end
+end
