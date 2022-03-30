8cbe33ca3a614496167e904632259b1d309fbfbc
Koichi Sasada <ko1@atdot.net>
2022-03-30 17:16:44 +0900

    Revert "flush trace output"
    This reverts commit 5c298b2034f62ad90eb51c0b8c7eb4256c837ab0.

--- lib/debug/tracer.rb
+++ lib/debug/tracer.rb
@@ -81,13 +81,11 @@ module DEBUGGER__
         ThreadClient.current.on_trace self.object_id, buff
       else
         @output.puts buff
-        @output.flush
       end
     end
 
     def puts msg
       @output.puts msg
-      @output.flush
     end
 
     def minfo tp
