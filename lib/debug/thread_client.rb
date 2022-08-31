9b4808c86c6269f7fc063df64b3ec637970d9bd9
Will Haltom <willhalto@github.com>
2022-08-31 15:14:49 +0000

    rename variable

--- lib/debug/thread_client.rb
+++ lib/debug/thread_client.rb
@@ -863,13 +863,13 @@ module DEBUGGER__
               loc = caller_locations(2, 1).first
               loc_path = loc.absolute_path || "!eval:#{loc.path}"
 
-              frame_depth = DEBUGGER__.frame_depth - 3
+              stack_depth = DEBUGGER__.frame_depth - 3
 
               # If we're at a deeper stack depth, we can skip line events until there's a return event.
-              skip_line = event == :line && frame_depth > depth
+              skip_line = event == :line && stack_depth > depth
 
               # same stack depth
-              (frame_depth <= depth) ||
+              (stack_depth <= depth) ||
 
               # different frame
               (next_line && loc_path == path &&
