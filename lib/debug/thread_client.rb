fa05ffa0814013434fbc39c241d1fc4ae5b95d93
Will Haltom <willhalto@github.com>
2022-09-01 20:03:50 +0000

    refactor for readability

--- lib/debug/thread_client.rb
+++ lib/debug/thread_client.rb
@@ -861,21 +861,19 @@ module DEBUGGER__
             step_tp iter do |event|
               next if event == :line && skip_line
 
-              loc = caller_locations(2, 1).first
-              loc_path = loc.absolute_path || "!eval:#{loc.path}"
-
               stack_depth = DEBUGGER__.frame_depth - 3
 
               # If we're at a deeper stack depth, we can skip line events until there's a return event.
               skip_line = event == :line && stack_depth > depth
 
               # same stack depth
-              (stack_depth <= depth) ||
+              next true if stack_depth <= depth
+
+              loc = caller_locations(2, 1).first
+              loc_path = loc.absolute_path || "!eval:#{loc.path}"
 
               # different frame
-              (next_line && loc_path == path &&
-               (loc_lineno = loc.lineno) > line &&
-               loc_lineno <= next_line)
+              next_line && loc_path == path && loc.lineno.between?(line + 1, next_line)
             end
             break
 
