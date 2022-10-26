74ca10233fa8059599279d5e4cd5e21a34176415
Koichi Sasada <ko1@atdot.net>
2022-10-26 17:46:00 +0900

    Revert "refactor for readability"
    This reverts commit fa05ffa0814013434fbc39c241d1fc4ae5b95d93.

--- lib/debug/thread_client.rb
+++ lib/debug/thread_client.rb
@@ -870,19 +870,21 @@ module DEBUGGER__
             step_tp iter do |event|
               next if event == :line && skip_line
 
+              loc = caller_locations(2, 1).first
+              loc_path = loc.absolute_path || "!eval:#{loc.path}"
+
               stack_depth = DEBUGGER__.frame_depth - 3
 
               # If we're at a deeper stack depth, we can skip line events until there's a return event.
               skip_line = event == :line && stack_depth > depth
 
               # same stack depth
-              next true if stack_depth <= depth
-
-              loc = caller_locations(2, 1).first
-              loc_path = loc.absolute_path || "!eval:#{loc.path}"
+              (stack_depth <= depth) ||
 
               # different frame
-              next_line && loc_path == path && loc.lineno.between?(line + 1, next_line)
+              (next_line && loc_path == path &&
+               (loc_lineno = loc.lineno) > line &&
+               loc_lineno <= next_line)
             end
             break
 
