14f7fdf56f173d275b875e001aa5b67c4f2d5aa2
Emily Samp <emily.giurleo@shopify.com>
2023-01-11 12:19:27 -0600

    Add test for instance variable ordering in DAP
    PR #806 sorts instance variables by name before returning them. This
    commit adds a test that verifies this functionality under the DAP
    protocol.

--- test/protocol/variables_test.rb
+++ test/protocol/variables_test.rb
@@ -3,7 +3,7 @@
 require_relative "../support/protocol_test_case"
 
 module DEBUGGER__
-  class DAPVariablesTest < ProtocolTestCase
+  class DAPGlobalVariablesTest < ProtocolTestCase
     PROGRAM = <<~RUBY
       1| $a = 1
       2| $b = 2
@@ -18,19 +18,19 @@ module DEBUGGER__
         globals = gather_variables(type: "globals")
 
         # User defined globals
-        assert_includes(globals, { name: "$a", value: "1", type: "Integer" })
-        assert_includes(globals, { name: "$b", value: "2", type: "Integer" })
+        assert_includes(globals, { name: "$a", value: "1", type: "Integer", variablesReference: 0 })
+        assert_includes(globals, { name: "$b", value: "2", type: "Integer", variablesReference: 0 })
 
         # Ruby defined globals
-        assert_includes(globals, { name: "$VERBOSE", value: "false", type: "FalseClass" })
-        assert_includes(globals, { name: "$stdout", value: "#<IO:<STDOUT>>", type: "IO" })
+        assert_includes(globals, { name: "$VERBOSE", value: "false", type: "FalseClass", variablesReference: 0 })
+        assert_includes(globals, { name: "$stdout", value: "#<IO:<STDOUT>>", type: "IO", variablesReference: 0 })
 
         req_terminate_debuggee
       end
     end
   end
 
-  class CDPVariablesTest < ProtocolTestCase
+  class CDPGlobalVariablesTest < ProtocolTestCase
     PROGRAM = <<~RUBY
       1| $a = 1
       2| $b = 2
@@ -51,6 +51,32 @@ module DEBUGGER__
         # Ruby defined globals
         assert_includes(globals, { name: "$VERBOSE", value: "false", type: "Boolean" })
         assert_includes(globals, { name: "$stdout", value: "#<IO:<STDOUT>>", type: "Object" })
+
+        req_terminate_debuggee
+      end
+    end
+  end
+
+  class DAPInstanceVariableTest < ProtocolTestCase
+    PROGRAM = <<~RUBY
+      1| @a = 1
+      2| @c = 3
+      3| @b = 2
+      4| __LINE__
+    RUBY
+
+    def test_ordering_instance_variables
+      run_protocol_scenario PROGRAM, cdp: false do
+        req_add_breakpoint 4
+        req_continue
+
+        locals = gather_variables
+
+        variables_reference = locals.find { |local| local[:name] == "%self" }[:variablesReference]
+        res = send_dap_request 'variables', variablesReference: variables_reference
+
+        instance_vars = res.dig(:body, :variables)
+        assert_equal instance_vars.map { |var| var[:name] }, ["#class", "@a", "@b", "@c"]
 
         req_terminate_debuggee
       end
