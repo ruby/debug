cb371de2539b5d620dfc918b330b7cee765e5dfc
Andy Waite <andyw8@users.noreply.github.com>
2023-02-22 16:10:54 -0500

    Use local variables

--- test/console/daemon_test.rb
+++ test/console/daemon_test.rb
@@ -4,19 +4,18 @@ require_relative '../support/console_test_case'
 
 module DEBUGGER__
   class DaemonTest < ConsoleTestCase
-    def program
+
+    def test_daemon
       # Ignore SIGHUP since the test debuggee receives SIGHUP after Process.daemon.
       # When manualy debugging a daemon, it doesn't receive SIGHUP.
       # I don't know why.
-      <<~'RUBY'
+      program = <<~'RUBY'
         1| trap(:HUP, 'IGNORE')
         2| puts 'Daemon starting'
         3| Process.daemon
         4| puts 'Daemon started'
       RUBY
-    end
 
-    def test_daemon
       # The program can't be debugged locally since the parent process exits when Process.daemon is called.
       debug_code program, remote: :remote_only do
         type 'b 3'
@@ -29,17 +28,15 @@ module DEBUGGER__
       end
     end
 
-    def program_with_daemon_arguments
-      <<~'RUBY'
+    def test_daemon_patch
+      program = <<~'RUBY'
         1| trap(:HUP, 'IGNORE')
         2| puts 'Daemon starting'
         3| Process.daemon(false, false)
         4| puts 'Daemon started'
       RUBY
-    end
 
-    def test_daemon_patch
-      debug_code program_with_daemon_arguments, remote: :remote_only do
+      debug_code program, remote: :remote_only do
         type 'b 3'
         type 'c'
         assert_line_num 3
