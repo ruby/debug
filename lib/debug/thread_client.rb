754ade2920228b987d25c1d81a5e7d0eb197bf1f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-10-21 14:19:49 +1300

    Support Ruby 3.1+.

--- lib/debug/thread_client.rb
+++ lib/debug/thread_client.rb
@@ -865,12 +865,15 @@ module DEBUGGER__
       private def fiber_blocking
         ::Fiber.blocking{yield}
       end
+    elsif ::Fiber.method_defined?(:blocking?)
+      private def fiber_blocking
+        ::Fiber.new(blocking: true){yield}.resume
+      end
     else
       private def fiber_blocking
         yield
       end
     end
-    
 
     def wait_next_action
       fiber_blocking{wait_next_action_}
